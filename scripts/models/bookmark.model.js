7d1d4517a4b7257afe5cbba907b04031ff006740
Vatsal Bharkhada <vatsal.bharkhada@simformsolutions.com>
2026-04-24 10:34:24 +0530

    Chore: remove test objects.

--- scripts/models/bookmark.model.js
+++ scripts/models/bookmark.model.js
@@ -11,15 +11,5 @@ const bookmark = {
     deletedAt: "",
     visits: 0
 }
-const bookmark1 = {
-    title: "First bookmark",
-    url: "https://google.com",
-    collections: ["First"],
-    tags: ["General"],
-    profile: "",
-    createdAt: new Date(),
-    deletedAt: "",
-    visits: 0
-}
 
 export { bookmark };

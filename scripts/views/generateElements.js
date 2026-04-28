98519d9d7f7af3ef8ed278ef28428bd76f300385
Vatsal Bharkhada <vatsal.bharkhada@simformsolutions.com>
2026-04-28 10:43:23 +0530

    Add: Styles for button type secondary.

--- scripts/views/generateElements.js
+++ scripts/views/generateElements.js
@@ -3,9 +3,9 @@ import tagColors from "./tagColors.js";
 function generateIconElement(name, className = "u-icon") {
     let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
     let useTag = document.createElementNS("http://www.w3.org/2000/svg", 'use');
+    svg.classList.add(className);
     useTag.setAttribute("href", `./assets/icons/sprite.svg#${name}`);
     svg.appendChild(useTag);
-    svg.classList.add(className);
     return svg;
 }
 
@@ -23,14 +23,7 @@ function generateTagElement(tagName) {
 }
 
 function generateDeletableTag(tagName) {
-    let tagElement = document.createElement('span');
-
-    tagElement.textContent = tagName;
-    tagElement.classList.add('u-tag');
-
-    tagElement.style.backgroundColor = tagColors[tagName[0].toUpperCase()]?.background || '#E0E0E0';
-    tagElement.style.color = tagColors[tagName[0].toUpperCase()]?.color || '#000000';
-    tagElement.style.borderColor = tagColors[tagName[0].toUpperCase()]?.border || '#E0E0E0';
+    let tagElement = generateTagElement(tagName);
 
     let closeButton = document.createElement('button');
     closeButton.classList.add("button-close-tag");

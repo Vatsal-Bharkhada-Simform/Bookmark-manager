7fc6a65d7fa1d8300efcd19dfb108b72a0d26e41
Vatsal Bharkhada <vatsal.bharkhada@simformsolutions.com>
2026-05-04 12:07:55 +0530

    Update: Add handlers to manage invalid paths. Create function to load default page.

--- scripts/events/addNavigationEvents.js
+++ scripts/events/addNavigationEvents.js
@@ -3,6 +3,50 @@ import { domElements } from "../views/domElements.js";
 let currentOpen = null;
 
 function addNavigationEvents() {
+    loadDefault();
+
+    domElements.sidebar.addEventListener("click", (e) => {
+        if (e.target.tagName === "A") {
+            let targetId = e.target.href.split("#")[1] ?? "";
+            let currentSelected = document.querySelector("[data-selected='true']");
+
+            if (currentSelected) {
+                currentSelected.dataset.selected = false;
+            }
+            e.target.dataset.selected = true;
+
+            if (targetId) {
+                let targetElement = document.querySelector(`#${targetId}`);
+
+                if (currentOpen) {
+                    currentOpen.style.display = "none";
+                    currentOpen = targetElement;
+                }
+
+                targetElement.style.display = "flex";
+            }
+        }
+    });
+
+    window.addEventListener('hashchange', function () {
+        let hashValue = this.window.location.href.split("#")[1] ?? "";
+        let targetElement = this.document.querySelector(`#${hashValue}`);
+
+        if(!targetElement){
+            console.log("Invalid: ", hashValue);
+            loadDefault();
+        } else {
+            if(targetElement === currentOpen) return;
+            else {
+                currentOpen.style.display = "none";
+                currentOpen = targetElement;
+                targetElement.style.display = "flex";
+            }
+        }
+    });
+}
+
+function loadDefault(){
     let defaultSelected = document.querySelector("[data-selected='true']");
     if (defaultSelected && defaultSelected.href) {
         let targetId = defaultSelected.href.split("#")[1] ?? "";
@@ -12,30 +56,9 @@ function addNavigationEvents() {
             targetElement.style.display = "flex";
             currentOpen = targetElement;
         }
+
+        window.location.href = `#${targetId}`;
     }
-
-    domElements.sidebar.addEventListener("click", (e) => {
-        if (e.target.tagName === "A") {
-            let targetId = e.target.href.split("#")[1] ?? "";
-            let currentSelected = document.querySelector("[data-selected='true']");
-            
-            if(currentSelected){
-                currentSelected.dataset.selected = false;
-            }
-            e.target.dataset.selected = true;
-
-            if (targetId) {
-                let targetElement = document.querySelector(`#${targetId}`);
-
-                if(currentOpen){
-                    currentOpen.style.display = "none";
-                    currentOpen = targetElement;    
-                }
-
-                targetElement.style.display = "flex";
-            }
-        }
-    });
 }
 
 export { addNavigationEvents };

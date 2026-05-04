d49e23f4aafce3e6848f8c946eed83096f3284ff
Vatsal Bharkhada <vatsal.bharkhada@simformsolutions.com>
2026-05-04 12:25:49 +0530

    Fix: Add error handling in connectDB function.

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,22 @@
 import { getAllBookmarks } from "./controllers/bookmark.controller.js";
 import { initDB } from "./controllers/db.controller.js";
-// let data = null;
 
-async function connectDB(){
-    await initDB();
-    let data = await getAllBookmarks();
+async function connectDB() {
+    try {
+        await initDB();
+    } catch (err) {
+        console.log("Error while connecting to database: ", err);
+        return;
+    }
+
+    let data;
+    try{
+        data = await getAllBookmarks();
+    } catch (err) {
+        console.log("Error in fetching data: ", err);
+        data = [];
+    }
+
     console.log(data);
 }
 

c8bcf64f145ed8c10be7a72aebfddfec9e5d8952
Vatsal Bharkhada <vatsal.bharkhada@simformsolutions.com>
2026-05-12 09:54:23 +0530

    Fix: Prevent form submission when invalid fields are present. Add tag and collection validation when added.

--- scripts/utils/bookmarkValidators.js
+++ scripts/utils/bookmarkValidators.js
@@ -12,7 +12,7 @@ function isValidText(input){
     return /^(?=.*\p{L}).+$/u.test(input);
 }
 
-function isValidForm(title, url, collections, tags){
+function isValidForm(title, url){
     if(title && !isValidText(title)){
         alert("Please provide a proper name.");
         return false;
@@ -21,18 +21,6 @@ function isValidForm(title, url, collections, tags){
         alert("Please provide a proper URL.");
         return false;
     }
-    collections.forEach(collection => {
-        if(!isValidText(collection)){
-            alert("Please provide a proper collection name.");
-            return false;
-        }
-    });
-    tags.forEach(tag => {
-        if(!isValidText(tag)){
-            alert("Please provide a proper tag name.");
-            return false;
-        }
-    });
     return true;
 }
 
